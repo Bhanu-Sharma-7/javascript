f382b0489623816ba85d1c91b65bca669212869b
Bhanu-Sharma-7 <bhanusharma14581@gmail.com>
2026-01-18 21:41:19 +0530

    var

---
+++ Phases 1/script.js
@@ -0,0 +1,16 @@
+// 1. var
+
+/* 
+
+var ES6 s pehle use hota tha variables ko decleared krne k liye but abhi hume let aur const ka use krte hai
+
+- block rules ko follow nahi krta hai
+- bugs bhi aate hai
+
+*/
+
+if(true) {
+    var a = 10
+    console.log(a)
+}
+console.log(a)
