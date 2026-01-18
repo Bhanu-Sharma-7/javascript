ae914d30902de42f17da1b1dd9e0624210f3bf56
Bhanu-Sharma-7 <bhanusharma14581@gmail.com>
2026-01-18 22:16:21 +0530

    arr and obj

---
+++ Phases 1/part3.js
@@ -0,0 +1,15 @@
+// arrays
+
+const data = ["Hello","Mera","naam","naam","hain"]
+
+console.log(data[1])
+
+// objects
+
+const data2 = {
+    name: "harry",
+    age: "age",
+    city: "city"
+}
+
+console.log(data2.name)
