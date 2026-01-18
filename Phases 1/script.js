4dd19e1cdef5bb6f8e693f5f1418a4c0ee2a9ef1
Bhanu-Sharma-7 <bhanusharma14581@gmail.com>
2026-01-18 21:48:04 +0530

    let

--- Phases 1/script.js
+++ Phases 1/script.js
@@ -9,8 +9,29 @@ var ES6 s pehle use hota tha variables ko decleared krne k liye but abhi hume le
 
 */
 
+console.log("var:")
+
 if(true) {
     var a = 10
     console.log(a)
 }
 console.log(a)
+
+// 2. let
+
+/*
+
+let ES6 m aaya tha isko var ka modify version bhi keh skte hai hum kyuki y var m jo bhi kamiya thi usko puri krta hai jese ki - block rules ko follow krna aur bugs bhi kam aate hai
+
+*/
+
+console.log("let:")
+
+if(true) {
+    let data = 20
+    console.log(data)
+}
+
+
+// console.log(data) /* yaha p humko ek chhota sa error dekhne ko milte hai jo let k karna aaya hai */
+
